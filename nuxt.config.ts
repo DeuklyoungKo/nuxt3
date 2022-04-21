21d02957465927d04712e77de2009ee51d95d1ef
DeuklyoungKo <lunaman@nexbrain.com>
2022-04-21 17:22:33 +0900

    Runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        // The private keys which are only available within server-side
+        apiSecret: '123',
+        NUXT_API_SECRET:process.env.NUXT_API_SECRET,
+        NUXT_PUBLIC_BASE_URL:process.env.NUXT_PUBLIC_BASE_URL,
+        // Keys within public, will be also exposed to the client-side
+        public: {
+          apiBase: '/api',
+          apiSecret2: '456',
+        }
+      }
 })
+
+
---
+++ pages/runtimeConfig.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>runtimeConfig
+    </h1>
+  </div>
+</template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+
+console.log("runtimeConfig")
+console.log(runtimeConfig)
+console.log("runtimeConfig.public")
+console.log(runtimeConfig.public)
+
+
+console.log("check process.server")
+if (process.server) {
+  console.log('API runtimeConfig:', runtimeConfig.apiSecret)
+  console.log('API NUXT_PUBLIC_BASE_URL:', runtimeConfig.NUXT_PUBLIC_BASE_URL)
+}
+
+</script>
